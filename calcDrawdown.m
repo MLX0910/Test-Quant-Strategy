a903c7fc756f550521dc8a89a98b0b6bf93abfc4
Nick <1114209005@qq.com>
2017-03-17 22:07:19 +0800

    Update calcDrawdown.m
    Add a line for test.

--- calcDrawdown.m
+++ calcDrawdown.m
@@ -4,5 +4,8 @@ max_perf = perf;
 for k = 1:length(perf)
     max_perf(k) = max(perf(1:k));
     drawdown(k) = perf(k) - max_perf(k);
+    
+    % add this line just for test
+    drawdown(1) = drawdown(1) + 1 - 1;
 end
 end
